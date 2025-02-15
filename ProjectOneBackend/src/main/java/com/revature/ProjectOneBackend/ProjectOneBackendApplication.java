1ced9f149aaab568845be12e995e27a95c05a377
= <andylackk@outlook.com>
2025-02-15 08:55:25 -0600

    Added Backend

---
+++ ProjectOneBackend/src/main/java/com/revature/ProjectOneBackend/ProjectOneBackendApplication.java
@@ -0,0 +1,14 @@
+package com.revature.ProjectOneBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectOneBackendApplication.class, args);
+		System.out.println("Project One Up and Running");
+	}
+
+}
