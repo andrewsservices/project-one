2600a03d0e547bc301e3f34b0fd6a1abe8b24e47
= <andylackk@outlook.com>
2025-02-16 09:46:20 -0600

    authcontroller

---
+++ ProjectOneBackend/src/main/java/com/revature/Controllers/AuthController.java
@@ -0,0 +1,22 @@
+package com.revature.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.models.Employee;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody Employee employee){
+
+        return ResponseEntity.ok("Employee registered, but not really");
+
+    }
+
+}
